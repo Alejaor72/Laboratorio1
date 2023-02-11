ff215ca4a5ce0c7663c6fd52b0717c6bc594948e
Your Name <you@example.com>
2023-02-10 19:38:27 -0500

    finish lab

---
+++ index.js
@@ -0,0 +1,9 @@
+const average=require('average');
+console.log(average);
+var result = average([2, 5, 1, 25, 7, 3, 0, 9, 10]);
+console.log('The average for all the values is:', result);
+console.log('////////////////////////////');
+
+const colors = require('colors');
+console.log('lab #1 sjsjsj!'.rainbow);
+
